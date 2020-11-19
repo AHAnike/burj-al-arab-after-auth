e0455eb95cf1a0f5627c132a0807145d474564e1
AHAnike <anikehasan2580@gmail.com>
2020-11-19 21:19:41 +0600

    burj al arab fast part

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,14 +1,75 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { UserContext } from '../../App';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+    KeyboardTimePicker
+} from '@material-ui/pickers';
 
 const Book = () => {
-    const {bedType} = useParams();
+    const { bedType } = useParams();
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [selectedDate, setSelectedDate] = useState({
+        checkIn: new Date (),
+        checkOut: new Date()
+    });
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
     return (
-        <div style={{textAlign: 'center'}}>
-            <h1>Let's book a {bedType} Room.</h1>
+        <div style={{ textAlign: 'center' }}>
+            <h1> Hello,{loggedInUser.name}! Let's book a {bedType} Room.</h1>
             <p>Want a <Link to="/home">different room?</Link> </p>
+
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justify="space-around">
+                    <KeyboardDatePicker
+                        disableToolbar
+                        variant="inline"
+                        format="MM/dd/yyyy"
+                        margin="normal"
+                        id="date-picker-inline"
+                        label="Check In Date"
+                        value={selectedDate.checkIn}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
+                    />
+                    <KeyboardDatePicker
+                        margin="normal"
+                        id="date-picker-dialog"
+                        label="Check Out Date"
+                        format="MM/dd/yyyy"
+                        value={selectedDate.checkOut}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
+                    />
+                    <KeyboardTimePicker
+                        margin="normal"
+                        id="time-picker"
+                        label="Time picker"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change time',
+                        }}
+                    />
+                </Grid>
+                <Button variant="contained" color="primary">Book Now</Button>
+
+
+            </MuiPickersUtilsProvider>
         </div>
     );
 };
 
-export default Book;
+export default Book; 
--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
-    authDomain: "burj-al-arab.firebaseapp.com",
-    databaseURL: "https://burj-al-arab.firebaseio.com",
-    projectId: "burj-al-arab",
-    storageBucket: "burj-al-arab.appspot.com",
-    messagingSenderId: "244522202387",
-    appId: "1:244522202387:web:f3801d0588387140e1d6e6"
-  };
+  apiKey: "AIzaSyC670KVWEX9QzssO4paizoZYZwzeaNL7Tc",
+  authDomain: "burj-al-arab-after-auth-488ab.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-after-auth-488ab.firebaseio.com",
+  projectId: "burj-al-arab-after-auth-488ab",
+  storageBucket: "burj-al-arab-after-auth-488ab.appspot.com",
+  messagingSenderId: "246259768582",
+  appId: "1:246259768582:web:45ab590b80598e9e149287"
+};
   export default firebaseConfig;
